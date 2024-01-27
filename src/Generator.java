6c6db1e40464705f6aaa651b91b2108ae1a53d1d
Crague Ilian <ilian.crague@etu.u-paris.com>
2024-01-27 11:14:07 +0100

    added result of reduction trame for a small image

--- src/Generator.java
+++ src/Generator.java
@@ -7,16 +7,16 @@ import java.io.File;
 public class Generator {
 
 	public static void main (String[] args) {
-		int sizeX = 50;
-		int sizeY = 50;
+		int sizeX = 40;
+		int sizeY = 40;
 		if(args.length == 2) {
 			sizeX = Integer.valueOf(args[0]);
 			sizeY = Integer.valueOf(args[1]);
 		}
 		BufferedImage image = new BufferedImage(sizeX, sizeY, BufferedImage.TYPE_BYTE_GRAY);
 		Random r = new Random();
-		for(int i = 0; i < 50; i++)
-		for(int j = 0; j < 50; j++) {
+		for(int i = 0; i < sizeX; i++)
+		for(int j = 0; j < sizeY; j++) {
 			int color = r.nextInt(256);
 			int rgb = (color << 16) + (color << 8) + color;
 			image.setRGB(i, j, rgb);
