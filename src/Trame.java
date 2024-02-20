22304e68f8b3ebcf99df53d20ed6ce5928543956
Crague Ilian <ilian.crague@etu.u-paris.com>
2024-02-20 15:27:35 +0100

    decalage du centre de la matrice un peu nul mais oklm

--- src/Trame.java
+++ src/Trame.java
@@ -5,6 +5,8 @@ import java.io.File;
 
 import javax.imageio.ImageIO;
 
+import java.util.Random;
+
 public class Trame {
 	public int width;
 	public int height;
@@ -60,7 +62,8 @@ public class Trame {
 		return image;
 	}
 
-	public static void main(String[] args) {
+	public static int [] gen_matrix(int centre) {
+		//centre correspond à l'indice du centre
 		int m[]=
 			{
 				15, 4, 8, 12,
@@ -68,19 +71,34 @@ public class Trame {
 				7, 2, 3, 9,
 				14, 10, 6, 13
 			};
+		int res [] = new int [16];
+		int decalage = (centre - 5 + 16) % 16;
+		for(int i = 0; i < 16; i++) {
+			res[(i + decalage) % 16] = m[i];
+		}
+		System.out.print("res : ");
+		for(int i = 0; i < 16; i++) {
+			System.out.print(res[i]+ " ");
+		}
+		System.out.println(";");
+		return res;
+	}
 
+	public static void main(String[] args) {
+		int m[] = gen_matrix(0);
 		Trame t = new Trame(4, 4, m);
+		for(int i = 1; i < 6; i++) {
+			String path = "images/image" + i + ".png";
+			File input_file = new File(path);
+			try {
+				BufferedImage image = ImageIO.read(input_file);
+				BufferedImage n = t.traming(image);
 
-		String path = "images/image5.png";
-		File input_file = new File(path);
-		try {
-			BufferedImage image = ImageIO.read(input_file);
-			BufferedImage n = t.traming(image);
-
-			File reduced = new File("output/gen-trame.png");
-			ImageIO.write(n, "png", reduced);
-		} catch(Exception e) {
-			e.printStackTrace();
+				File reduced = new File("output/trame/gen-trame" + i + ".png");
+				ImageIO.write(n, "png", reduced);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
