13ec53d5d2ba47d78e222007a3dd8986863fb6d3
Crague Ilian <ilian.crague@etu.u-paris.com>
2024-03-25 19:51:52 +0100

    added naive tests

--- src/ReduceGrayTrameIrrDisp.java
+++ src/ReduceGrayTrameIrrDisp.java
@@ -8,34 +8,34 @@ import java.util.HashSet;
 
 public class ReduceGrayTrameIrrDisp {
 
-	public static int [] genPattern(int nWhite) {
-	//nWhite is the number of white pixels we need to place in res
+	public static int [] getNewPixel(int nWhite) {
+		//nWhite is the number of white pixels we need to place in res
 		Random r = new Random();
 		int w = 0xFFFFFF;
 		int b = 0;
 		int res[] =
-				{
-					b, b, b, b,
-					b, b, b, b,
-					b, b, b, b,
-					b, b, b, b
-				};
-				
+		{
+			b, b, b, b,
+			b, b, b, b,
+			b, b, b, b,
+			b, b, b, b
+		};
+
 		Set<Integer> toWhite = new HashSet<>();
 		while(toWhite.size() < nWhite) {
 			toWhite.add(r.nextInt(16));
 		}
-		
+
 		for(Integer index : toWhite) {
 			res[index] = w;
 		}
-		
+
 		return res;
 	}
 
 	public static void main (String[] args) {
-			
-BufferedImage image;
+
+		BufferedImage image;
 		String path = "images/farwest.png";
 		File input_file = new File(path);
 		try {
@@ -51,12 +51,12 @@ BufferedImage image;
 				for(int j = 0; j < t_y; j++) {
 					int tot = 0;
 					for(int x = 0; x < 4; x++)
-					for(int y = 0; y < 4; y++) {
-						tot += image.getRGB(4 * i + x, 4 * j + y) & 0xFF;
-					}
+						for(int y = 0; y < 4; y++) {
+							tot += image.getRGB(4 * i + x, 4 * j + y) & 0xFF;
+						}
 					System.out.println(tot / 16 / 16);
 
-						image.setRGB(4 * i, 4 * j, 4, 4, genPattern(tot / 16 / 16), 0, 4);
+					image.setRGB(4 * i, 4 * j, 4, 4, getNewPixel(tot / 16 / 16), 0, 4);
 				}
 
 			File reduced = new File("output/reduced-image-irr-disp.png");
